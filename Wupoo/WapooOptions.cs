f409e3b94fcde0c20ed548df4adc8e94278bb3ce
ac682 <2419328026@qq.com>
2020-09-20 00:22:08 +0800

    添加项目文件。

---
+++ Wupoo/WapooOptions.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Wupoo
+{
+    public class WapooOptions
+    {
+        public AuthenticationHeaderValue Authentication { get; set; }
+        public bool IgnoreMediaTypeCheck { get; set; }
+        public JsonSerializerSettings JsonSerializerOptions { get; set; }
+    }
+}
